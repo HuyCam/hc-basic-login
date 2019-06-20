5e4ff4f24374eb9c66c16a9aad86bca036aa9414
HuyCam <camhuy165@gmail.com>
2019-06-20 17:24:05 -0500

    fix bug

--- src/public/main.js
+++ src/public/main.js
@@ -1,4 +1,4 @@
-const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1Y2YzMzZmMWY2NGUxNjAwMTcwNjJmYmEiLCJpYXQiOjE1NjEwMTE4MTksImV4cCI6MTU2MTAxMjExOX0.LRIKl_NsNtKQ7aHhR9nWTVTF8aKMLZfvUFn92QHDkPk';
+const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1Y2YzMzZmMWY2NGUxNjAwMTcwNjJmYmEiLCJpYXQiOjE1NjEwNjkxNTYsImV4cCI6MTU2MTA2OTQ1Nn0.q4MZOrT7iCq2hO6S6l1Aaa1eISIp6FGwH-_55IIxQAo';
 
 const form = document.querySelector('#form-avatar');
 const input = document.querySelector('#img-file');
@@ -10,28 +10,28 @@ input.addEventListener('change', function(e) {
 form.addEventListener('submit', function(e) {
     console.log('Starting to send image');
     e.preventDefault();
-    // let formData = new FormData();
-    // formData.append("avatar", input.files[0]);
-    window.location.href='http://localhost:3001/login.html';
-    // $.ajax({
-    //     async: true,
-    //     crossDomain: true,
-    //     url: 'https://hc-basic-login.herokuapp.com/users/me/avatar',
-    //     method: 'POST',
-    //     data: formData,
-    //     processData: false,
-    //     contentType: false,
-    //     mimeType: "multipart/form-data",
-    //     headers: {
-    //         'Authorization': 'Bearer '+ TOKEN
-    //     },
-    //     success: function(data, textStatus) {
-    //         console.log(data, textStatus);
-    //     },
-    //     error: function(e) {
-    //         console.log(e);
-    //     }
-    // });
+    let formData = new FormData();
+    formData.append("avatar", input.files[0]);
+
+    $.ajax({
+        async: true,
+        crossDomain: true,
+        url: 'https://hc-basic-login.herokuapp.com/users/me/avatar',
+        method: 'POST',
+        data: formData,
+        processData: false,
+        contentType: false,
+        mimeType: "multipart/form-data",
+        headers: {
+            'Authorization': 'Bearer '+ TOKEN
+        },
+        success: function(data, textStatus) {
+            console.log(data, textStatus);
+        },
+        error: function(e) {
+            console.log(e);
+        }
+    });
 
 
     // var form = new FormData();
